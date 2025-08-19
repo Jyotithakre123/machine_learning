cb723081877ca7aab093427f7f9592fed48384ae
Jyoti Thakre <jyotithakre0099@gmail.com>
2025-08-19 10:05:26 +0530

    bank_loan_commit

--- Bank_loan_eligiblity/app3.py
+++ Bank_loan_eligiblity/app3.py
@@ -5,7 +5,7 @@ import numpy as np
 with open("bank_loan_model.pkl", 'rb') as f:
     model = pickle.load(f)
 
-st.title("Bank Loan Eligibility Predictor")
+st.title("Bank Loan Eligibility")
 
 Gender = st.selectbox("Gender", ["Male", "Female"])
 Married = st.selectbox("Married", ["Yes", "No"])
@@ -13,16 +13,19 @@ Dependents = st.number_input("Dependents", step=1, min_value=0)
 Education = st.selectbox("Education", ["Graduate", "Not Graduate"])
 Self_Employed = st.selectbox("Self Employed", ["Yes", "No"])
 ApplicantIncome = st.number_input("Applicant Income", step=500)
-CoapplicantIncome = st.number_input("Coapplicant Income", step=500)
 LoanAmount = st.number_input("Loan Amount", step=10)
 Loan_Amount_Term = st.number_input("Loan Amount Term (in days)", step=12)
 Credit_History = st.selectbox("Credit History", [1.0, 0.0])
-Property_Area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
+
+gender_map = {"Male": 0, "Female": 1}
+married_map = {"Yes": 1, "No": 0}
+education_map = {"Graduate": 1, "Not Graduate": 0}
+self_employed_map = {"Yes": 1, "No": 0}
 
 if st.button("Check Eligibility"):
     input_data = np.array([[Gender, Married, Dependents, Education, Self_Employed,
-                            ApplicantIncome, CoapplicantIncome, LoanAmount,
-                            Loan_Amount_Term, Credit_History, Property_Area]])
+                            ApplicantIncome, LoanAmount,
+                            Loan_Amount_Term, Credit_History]])
 
     prediction = model.predict(input_data)[0]
 
